510df749e450c070e731d9f3bc2787fa26d78e58
LorenzoLG <lorenzolg@estudante.ufscar.br>
2025-05-14 19:04:34 -0300

    formatação de texto

--- cenas/jogador.gd
+++ cenas/jogador.gd
@@ -3,38 +3,57 @@ extends Node2D
 
 class_name Jogador
 
-var Almas: int
-var Dinheiro: int
-var Fama: float
-var Inventario_Funcionarios: Array = []
-var Inventario_Maquinas: Array = []
-var Inventario_Upgrades: Array = []
-var Inventario_Achivements: Array = []
+var almas: int
+var dinheiro: int
+var fama: float
+var inventario_funcionarios: Array = [Funcionario]
+var inventario_maquinas: Array = [Maquina]
+var inventario_upgrades: Array = []
+var inventario_achivements: Array = []
 
-func add_dinheiro(modificar: int) -> void:
-	Dinheiro = Dinheiro + modificar
+var atributos = []
+
+func _ready() -> void:
+	atributos = [almas,dinheiro,fama,inventario_funcionarios,inventario_maquinas,inventario_upgrades,inventario_achivements]
+	
 	pass
 
-func alterar_almas(modificar: int) -> void:
-	Almas = Almas + modificar
+func add_dinheiro(adicionar: int) -> void:
+	dinheiro = dinheiro + adicionar
 	pass
 
-func alterar_fama(modificar: float) -> void:
-	Fama = Fama + modificar
+func sub_dinheiro(subtrair: int) -> void:
+	dinheiro = dinheiro - subtrair
+	pass
+
+func add_almas(adicionar: int) -> void:
+	almas = almas + adicionar
+	pass
+
+func sub_almas(subtrair: int) -> void:
+	almas = almas - subtrair
+	pass
+
+func add_fama(adicionar: float) -> void:
+	fama = fama + adicionar
+	pass
+
+func sub_fama(subtrair: float) -> void:
+	fama = fama - subtrair
 	pass
 
 func adicionar_inventario_funcionarios(funcionario_selecionado: Funcionario) -> void:
-	Inventario_Funcionarios.append(funcionario_selecionado)
+	inventario_funcionarios.append(funcionario_selecionado)
 	pass
 
 func retirar_inventario_funcionarios(funcionario_selecionado: Funcionario) -> void:
-	Inventario_Funcionarios.erase(funcionario_selecionado)
+	inventario_funcionarios.erase(funcionario_selecionado)
 	pass
 
 func adicionar_inventario_maquinas(maquina_selecionada: Maquina) -> void:
-	Inventario_Maquinas.append(maquina_selecionada)
+	inventario_maquinas.append(maquina_selecionada)
 	pass
 
 func retirar_inventario_maquinas(maquina_selecionada: Maquina) -> void:
-	Inventario_Maquinas.erase(maquina_selecionada)
+	inventario_maquinas.erase(maquina_selecionada)
 	pass
