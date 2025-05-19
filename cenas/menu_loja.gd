1a7239980b3264ad122bd90590865f897bb0c998
LorenzoLG <lorenzolg@estudante.ufscar.br>
2025-05-19 13:04:13 -0300

    loja ui primeiro draft

--- cenas/menu_loja.gd
+++ cenas/menu_loja.gd
@@ -1,24 +1,12 @@
 # TabContainer.gd
 extends TabContainer
 
-# Simulando dados diferentes para cada aba
-var items_por_aba = [
-	["Espada", "Arco", "Martelo"],
-	["Poção", "Elixir", "Bomba"],
-	["Mapa", "Chave", "Lanterna"]
-]
+@onready var grade_funcionario = $Funcionarios/ScrollContainer/GridContainer
+@onready var tab_maquina = $Maquinas
+var cenabotao = preload("res://cenas/botao.tscn")
 
-func _ready():
-	for i in range(get_child_count()):
-		var scroll = get_child(i)
-		var vbox = scroll.get_child(0)
-		vbox.clear()  # Limpa qualquer conteúdo anterior
-
-		for nome_item in items_por_aba[i]:
-			var botao = Button.new()
-			botao.text = nome_item
-			botao.pressed.connect(func(): _on_item_clicado(nome_item))
-			vbox.add_child(botao)
-
-func _on_item_clicado(nome_item):
-	print("Você clicou no item:", nome_item)
+func _ready() -> void:
+	
+	for n in range(10):
+		var newbotao = cenabotao.instantiate()
+		grade_funcionario.add_child(newbotao)
