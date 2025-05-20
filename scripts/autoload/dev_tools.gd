7efaf6346d33d39f107fd2dfeb59d8887458a2cc
DeathArcanaXIIIP3 <deatharcana@hotmail.com>
2025-05-19 23:03:07 -0300

    Dev_Tools e Input Handler

--- scripts/autoload/dev_tools.gd
+++ scripts/autoload/dev_tools.gd
@@ -1,8 +1,11 @@
 extends Control
 
-
+var viewport
+var screenSize
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	viewport = get_viewport_rect()
+	screenSize = viewport.size
 	pass # Replace with function body.
 
 func _on_button_pressed() -> void:
@@ -18,8 +21,10 @@ func factory_funcionario(data: FuncionarioData):
 	var funcionario = preload("res://cenas/funcionario.tscn").instantiate()
 	add_child(funcionario)
 	funcionario.inicializacao(data)
+	funcionario.global_position = Vector2(screenSize[0] / 2, screenSize[1] / 2)
 
 func factory_maquina(data: MaquinaData):
 	var maquina = preload("res://cenas/maquina.tscn").instantiate()
 	add_child(maquina)
 	maquina.setup(data)
+	maquina.global_position = Vector2(screenSize[0] / 2, screenSize[1] / 2)
